9382a5c7d1ce1599168b1e69a8c8db4601753988
Ikart-1 <ilyasskarroum54@gmail.com>
2023-06-15 10:23:25 +0100

    bb

--- resources/views/admin/body.blade.php
+++ resources/views/admin/body.blade.php
@@ -1,94 +1,90 @@
+<?php
+
+// Contrôleur Appointment
+use App\Models\Appointment;
+
+$appointmentsCount = Appointment::count();
+
+// Contrôleur User
+use App\Models\User;
+
+$usersCount = User::count();
+
+// Contrôleur Doctor
+use App\Models\Doctor;
+
+$doctorsCount = Doctor::count();
+?>
+
 <div class="container-fluid py-4">
-      <div class="row">
-        <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
-          <div class="card">
+<div class="row">
+    <div class="col-xl-4 col-sm-6 mb-xl-0 mb-4">
+        <div class="card">
             <div class="card-body p-3">
-              <div class="row">
-                <div class="col-8">
-                  <div class="numbers">
-                    <p class="text-sm mb-0 text-capitalize font-weight-bold">Today's Money</p>
-                    <h5 class="font-weight-bolder mb-0">
-                      $53,000
-                      <span class="text-success text-sm font-weight-bolder">+55%</span>
-                    </h5>
-                  </div>
+                <div class="row">
+                    <div class="col-8">
+                        <div class="numbers">
+                            <p class="text-sm mb-0 text-capitalize font-weight-bold">Nombre de patients</p>
+                            <h5 class="font-weight-bolder mb-0">
+                                <?php echo $usersCount; ?>
+                            </h5>
+                        </div>
+                    </div>
+                    <div class="col-4 text-end">
+                        <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md"
+                            style=" background-image: linear-gradient(310deg, #2152ff 0%, #21d4fd 100%);">
+                            <i class="fas fa-hospital-user text-lg opacity-10" aria-hidden="true"></i>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-4 text-end">
-                  <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md" style=" background-image: linear-gradient(310deg, #2152ff 0%, #21d4fd 100%);">
-                    <i class="ni ni-money-coins text-lg opacity-10" aria-hidden="true"></i>
-                  </div>
-                </div>
-              </div>
             </div>
-          </div>
         </div>
-        <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
-          <div class="card">
+    </div>
+    <div class="col-xl-4 col-sm-6 mb-xl-0 mb-4">
+        <div class="card">
             <div class="card-body p-3">
-              <div class="row">
-                <div class="col-8">
-                  <div class="numbers">
-                    <p class="text-sm mb-0 text-capitalize font-weight-bold">Today's Users</p>
-                    <h5 class="font-weight-bolder mb-0">
-                      2,300
-                      <span class="text-success text-sm font-weight-bolder">+3%</span>
-                    </h5>
-                  </div>
+                <div class="row">
+                    <div class="col-8">
+                        <div class="numbers">
+                            <p class="text-sm mb-0 text-capitalize font-weight-bold">Nombre de rendez-vous</p>
+                            <h5 class="font-weight-bolder mb-0">
+                                <?php echo $appointmentsCount; ?>
+                            </h5>
+                        </div>
+                    </div>
+                    <div class="col-4 text-end">
+                        <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md"
+                            style=" background-image: linear-gradient(310deg, #2152ff 0%, #21d4fd 100%);">
+                            <i class="far fa-calendar text-lg opacity-10" aria-hidden="true"></i>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-4 text-end">
-                  <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md" style=" background-image: linear-gradient(310deg, #2152ff 0%, #21d4fd 100%);">
-                    <i class="ni ni-world text-lg opacity-10" aria-hidden="true"></i>
-                  </div>
-                </div>
-              </div>
             </div>
-          </div>
         </div>
-        <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
-          <div class="card">
+    </div>
+    <div class="col-xl-4 col-sm-6">
+        <div class="card">
             <div class="card-body p-3">
-              <div class="row">
-                <div class="col-8">
-                  <div class="numbers">
-                    <p class="text-sm mb-0 text-capitalize font-weight-bold">New Clients</p>
-                    <h5 class="font-weight-bolder mb-0">
-                      +3,462
-                      <span class="text-danger text-sm font-weight-bolder">-2%</span>
-                    </h5>
-                  </div>
+                <div class="row">
+                    <div class="col-8">
+                        <div class="numbers">
+                            <p class="text-sm mb-0 text-capitalize font-weight-bold">Nombre de médecins</p>
+                            <h5 class="font-weight-bolder mb-0">
+                                <?php echo $doctorsCount; ?>
+                            </h5>
+                        </div>
+                    </div>
+                    <div class="col-4 text-end">
+                        <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md"
+                            style=" background-image: linear-gradient(310deg, #2152ff 0%, #21d4fd 100%);">
+                            <i class="fas fa-stethoscope text-lg opacity-10" aria-hidden="true"></i>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-4 text-end">
-                  <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md" style=" background-image: linear-gradient(310deg, #2152ff 0%, #21d4fd 100%);">
-                    <i class="ni ni-paper-diploma text-lg opacity-10" aria-hidden="true"></i>
-                  </div>
-                </div>
-              </div>
             </div>
-          </div>
         </div>
-        <div class="col-xl-3 col-sm-6">
-          <div class="card">
-            <div class="card-body p-3">
-              <div class="row">
-                <div class="col-8">
-                  <div class="numbers">
-                    <p class="text-sm mb-0 text-capitalize font-weight-bold">Sales</p>
-                    <h5 class="font-weight-bolder mb-0">
-                      $103,430
-                      <span class="text-success text-sm font-weight-bolder">+5%</span>
-                    </h5>
-                  </div>
-                </div>
-                <div class="col-4 text-end">
-                  <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md" style=" background-image: linear-gradient(310deg, #2152ff 0%, #21d4fd 100%);">
-                    <i class="ni ni-cart text-lg opacity-10" aria-hidden="true"></i>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+    </div>
+</div>
 
      
       <div class="row mt-4">
