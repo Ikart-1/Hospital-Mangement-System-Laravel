b22525deb5c9d1423e8fcc9bb72d28a6adc2d9c9
Ikart-1 <ilyasskarroum54@gmail.com>
2023-06-19 10:34:07 +0100

    Done

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AppointmentController;
 
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
